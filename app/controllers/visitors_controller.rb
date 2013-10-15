d517544490ac8c8d084385210bfdce492b89402e
hicktaf <hicktaf@gmail.com>
2013-10-15 11:27:02 +0200

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -4,6 +4,8 @@ class VisitorsController < ApplicationController
       # Model
       @owner = Owner.new
       # View
+      #flash.now[:notice] = 'Welcome!'
+      #flash.now[:alert] = 'My birthday is soon.'
       # render 'visitors/new' inherited from ApplicationController
     end
 
